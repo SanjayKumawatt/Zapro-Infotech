d90354adc52675213576a60da4518a1a656105eb
SanjayKumawatt <sk9785699310@gmail.com>
2026-04-27 14:43:30 +0530

    message

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Header from '../Components/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Components/Footer'
+import ScrollToTop from '../Components/Scrolltotop'
 
 const AppLayout = () => {
   return (
     <div>
-        
+        <ScrollToTop/>
         <Header/>
         <Outlet/>
         <Footer/>
